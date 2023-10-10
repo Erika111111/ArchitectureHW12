dda14ce8b55d02ef56ab5170e1c857ee6251a56d
Erika <Erika1111@yandex.ru>
2023-10-10 22:17:07 +0300

    HW12

---
+++ HW12/src/ClientProtocol.java
@@ -0,0 +1,15 @@
+public class ClientProtocol extends LinkProtocol {
+
+    public static void processInput(ConnectionMessage m) {
+        Messanger.setState(m);
+    }
+
+    public static ConnectionMessage getOutput() {
+//        if (!messages.empty())
+//            JOptionPane.showMessageDialog(Messanger.getFrame(), "Message sent");
+        ConnectionMessage m = new ConnectionMessage(messages, null, Messanger.getName());
+        messages.clear();
+        return m;
+    }
+
+}
---
+++ HW12/src/LinkProtocol.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+public class LinkProtocol {
+
+    static {
+        messages = new Stack<Message>();
+    }
+
+    protected static Stack<Message> messages;
+    public static void sendMessage(Message m) {
+        messages.push(m);
+    }
+
+}
